c60c739a84d19d75969fa85ed15750c802572835
KesslerCascade <117657415+KesslerCascade@users.noreply.github.com>
2022-11-10 07:51:22 -0600

    1.1 Release

--- xturbtester.h
+++ xturbtester.h
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <tchar.h>
 
-#define PROGNAME "XTurbTester 1.0"
-#define PROGNAMEW L"XTurbTester 1.0"
+#define PROGNAME "XTurbTester 1.1"
+#define PROGNAMEW L"XTurbTester 1.1"
 
 typedef long long int64;
